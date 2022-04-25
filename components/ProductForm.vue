e5192ca40c2673a5d68e38f441e62d163781b728
Maksim Basov <maksimbasov1992@mail.ru>
2022-04-25 18:21:31 +0300

    feat: initial

---
+++ components/ProductForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="product-form">
+    <h2 class="product-form__title">
+      Добавление товара
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductForm',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.product-form {
+    .product-form__title {
+        margin:0;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <AppLayout>
+      <ProductForm />
+    </AppLayout>
+  </div>
+</template>
+
+<script>
+import AppLayout from '../layouts/AppLayout'
+import ProductForm from '../components/ProductForm.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    AppLayout,
+    ProductForm
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.main {
+  background: #E5E5E5;
+}
+</style>
